10f06c6fc3ca5a1e5ab76a770bd159a7d9dad508
vineetkpe <vineetkpe@gmail.com>
2026-08-09 12:32:47 +0530

    latest push

--- packages/database/index.ts
+++ packages/database/index.ts
@@ -2,4 +2,4 @@ export { createBrowserClient } from './src/client';
 export { createServerClient, createTokenClient, type CookieMethods } from './src/server';
 export { getSupabaseEnv, hasSupabaseEnv, type SupabaseEnv } from './src/env';
 export type { Database, Json } from './src/types';
-export type { SupabaseClient, User, Session } from '@supabase/supabase-js';
+export { createClient, type SupabaseClient, type User, type Session } from '@supabase/supabase-js';
